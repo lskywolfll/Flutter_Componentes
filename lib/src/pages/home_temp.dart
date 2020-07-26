38b37c9ad8dcddc911ab843441a33542593e86c1
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-07-25 20:16:41 -0400

    se agrega un list view

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dow', 'Tres', 'Cuatro', 'Cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: _crearItems(),
+      ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorto() {}
+}
