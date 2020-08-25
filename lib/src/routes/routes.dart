a662d285232b146a99bdc6035911e523087906e5
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-08-24 20:52:49 -0400

    se agregan sliders como componentes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/card_page.dart';
@@ -14,6 +15,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimateContainerPage(),
-    'Inputs': (BuildContext context) => InputPage()
+    'Inputs': (BuildContext context) => InputPage(),
+    'slider': (BuildContext context) => SliderPage()
   };
 }
