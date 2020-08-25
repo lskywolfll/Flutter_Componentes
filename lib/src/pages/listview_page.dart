4c62151fba6edc944a4d3c70fdf868fce121b959
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-08-25 00:16:24 -0400

    se crean unas funcionalidades para llamar imagenes

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -6,7 +6,20 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _listaNumeros = [1, 2, 3, 4];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _agregar5();
+
+    _scrollController.addListener(() {
+      print('scroll!!');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +34,7 @@ class _ListaPageState extends State<ListaPage> {
   Widget _crearLista() {
     // Renderizar elementos cuantos sean necesarios por eso se utiliza el builder
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (context, index) {
         final imagen = _listaNumeros[index];
@@ -31,5 +45,14 @@ class _ListaPageState extends State<ListaPage> {
         );
       },
     );
+  }
+
+  void _agregar5() {
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+
+    setState(() {});
   }
 }
